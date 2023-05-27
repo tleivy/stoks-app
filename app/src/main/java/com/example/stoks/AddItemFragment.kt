299ce2713d1d5f332e93463e72741fa9ce07ab2a
tleivy <tomerleivy1@gmail.com>
2023-05-27 22:20:49 +0300

    feat: select stock name list

--- app/src/main/java/com/example/stoks/AddItemFragment.kt
+++ app/src/main/java/com/example/stoks/AddItemFragment.kt
@@ -10,6 +10,8 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
@@ -50,41 +52,69 @@ class AddItemFragment : Fragment() {
         val stockImages = stockDataMaps.stockImages
         var currPrice = 0.0
 
-
-        binding.stockName.addTextChangedListener(object : TextWatcher {
-            override fun onTextChanged(
-                newName: CharSequence?,
-                start: Int,
-                before: Int,
-                count: Int
-            ) {
-                val companyName = newName.toString()
+        val stockNames = stockSymbols.keys.toList()
+        val namesAdapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item, stockNames)
+        val spinner = binding.namesSpinner
+        spinner.adapter = namesAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val companyName = stockNames[p2]
                 if (stockSymbols[companyName] != null) {
                     binding.stockSymbol.setText(stockSymbols[companyName])
                     binding.previewImage.setImageResource(stockImages[companyName]!!)
+
+                    // TODO: Needs to be an API call
+                    val pricesArr = stockPrices[companyName]
+                    if (pricesArr != null) {
+                        val randomInt = Random.nextInt(3)  // A random int in 0-2
+                        currPrice = pricesArr?.get(randomInt) ?: 0.0
+                        binding.stockPrice.setText(currPrice?.toString())
+                    }
                 } else {
                     // TODO: API call -> get stock symbol, add to map
                 }
-
-                // TODO: Needs to be an API call
-                val pricesArr = stockPrices[companyName]
-                if (pricesArr != null) {
-                    val randomInt = Random.nextInt(3)  // A random int in 0-2
-                    currPrice = pricesArr?.get(randomInt) ?: 0.0
-                    binding.stockPrice.setText(currPrice?.toString())
-                }
             }
 
-            override fun beforeTextChanged(
-                newName: CharSequence?,
-                start: Int,
-                count: Int,
-                after: Int
-            ) {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
             }
 
-            override fun afterTextChanged(newName: Editable?) {}
-        })
+        }
+
+//        binding.stockName.addTextChangedListener(object : TextWatcher {
+//            override fun onTextChanged(
+//                newName: CharSequence?,
+//                start: Int,
+//                before: Int,
+//                count: Int
+//            ) {
+//                val companyName = newName.toString()
+//                if (stockSymbols[companyName] != null) {
+//                    binding.stockSymbol.setText(stockSymbols[companyName])
+//                    binding.previewImage.setImageResource(stockImages[companyName]!!)
+//                } else {
+//                    // TODO: API call -> get stock symbol, add to map
+//                }
+//
+//                // TODO: Needs to be an API call
+//                val pricesArr = stockPrices[companyName]
+//                if (pricesArr != null) {
+//                    val randomInt = Random.nextInt(3)  // A random int in 0-2
+//                    currPrice = pricesArr?.get(randomInt) ?: 0.0
+//                    binding.stockPrice.setText(currPrice?.toString())
+//                }
+//            }
+//
+//            override fun beforeTextChanged(
+//                newName: CharSequence?,
+//                start: Int,
+//                count: Int,
+//                after: Int
+//            ) {
+//            }
+//
+//            override fun afterTextChanged(newName: Editable?) {}
+//        })
 
         binding.stockAmount.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(
