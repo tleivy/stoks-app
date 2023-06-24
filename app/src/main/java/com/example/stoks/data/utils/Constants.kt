1cc7e719935530c663202f326f86f441ea079f8d
tleivy <tomerleivy1@gmail.com>
2023-06-24 14:39:52 +0300

    feat: changed api key

--- app/src/main/java/com/example/stoks/data/utils/Constants.kt
+++ app/src/main/java/com/example/stoks/data/utils/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
 
     companion object {
         const val BASE_URL = "https://finnhub.io/api/v1/"
-        const val API_KEY = "cia91q9r01qn522buad0cia91q9r01qn522buadg"
+        const val API_KEY = "ci7hvuhr01qni8lh9vr0ci7hvuhr01qni8lh9vrg"
         const val SYMBOL_QUERY_PARAM = "symbol"
         const val TOKEN_QUERY_PARAM = "token"
     }
