5d831ac5e31703c927ba9872e309540be344290a
tleivy <tomerleivy1@gmail.com>
2023-07-28 12:04:31 +0300

    deleted redundent constants

--- app/src/main/java/com/example/stoks/data/utils/Constants.kt
+++ app/src/main/java/com/example/stoks/data/utils/Constants.kt
@@ -7,7 +7,5 @@ class Constants {
     companion object {
         const val BASE_URL = "https://finnhub.io/api/v1/"
         const val API_KEY = BuildConfig.FINNHUB_API_KEY
-        const val SYMBOL_QUERY_PARAM = "symbol"
-        const val TOKEN_QUERY_PARAM = "token"
     }
 }
