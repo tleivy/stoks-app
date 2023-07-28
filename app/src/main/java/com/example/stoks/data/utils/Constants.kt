c633c8e12f7e965e383d6d98a5ad23ceae0a19e5
tleivy <tomerleivy1@gmail.com>
2023-07-28 11:56:05 +0300

    changed api key to build constant

--- app/src/main/java/com/example/stoks/data/utils/Constants.kt
+++ app/src/main/java/com/example/stoks/data/utils/Constants.kt
@@ -1,11 +1,12 @@
 package com.example.stoks.data.utils
 
+import com.example.stoks.BuildConfig
 
 class Constants {
 
     companion object {
         const val BASE_URL = "https://finnhub.io/api/v1/"
-        const val API_KEY = "ci7hvuhr01qni8lh9vr0ci7hvuhr01qni8lh9vrg"
+        const val API_KEY = BuildConfig.FINNHUB_API_KEY
         const val SYMBOL_QUERY_PARAM = "symbol"
         const val TOKEN_QUERY_PARAM = "token"
     }
