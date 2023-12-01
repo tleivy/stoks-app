b58cc704f632157fdcc7f131ba7dc5dbcd75927d
tleivy <tomerleivy1@gmail.com>
2023-12-01 17:12:02 +0200

    refactor, added new function

--- app/src/main/java/com/example/stoks/data/repository/StockRepository.kt
+++ app/src/main/java/com/example/stoks/data/repository/StockRepository.kt
@@ -2,45 +2,38 @@ package com.example.stoks.data.repository
 
 import com.example.stoks.data.local.StockDao
 import com.example.stoks.data.model.Stock
+import com.example.stoks.data.model.StockRemoteModel
 import com.example.stoks.data.remote_db.StockRemoteDataSource
+import com.example.stoks.data.utils.Resource
 import javax.inject.Inject
 
 
 class StockRepository @Inject constructor(
     private val stockDao: StockDao,
     private val remoteDataSource: StockRemoteDataSource
-    ) {
-
-    fun getItems() = stockDao.getItems()
+) {
+    fun getItems() = stockDao.getAllStocks()
 
     fun getFavorites() = stockDao.getFavorites()
 
-    suspend fun addItem(stock: Stock) {
-        stockDao.addItem(stock)
-    }
+    suspend fun addItem(stock: Stock) = stockDao.addNewStock(stock)
 
-    suspend fun deleteItem(stock: Stock) {
-        stockDao.deleteItem(stock)
-    }
+    suspend fun deleteItem(stock: Stock) = stockDao.deleteStock(stock)
 
-    suspend fun deleteAll() {
-        stockDao.deleteAll()
-    }
+    suspend fun deleteAll() = stockDao.deleteAll()
 
     suspend fun removeFromFavorites(stock: Stock) {
         stock.isFavorite = false
-        stockDao.updateItem(stock)
+        stockDao.updateStock(stock)
     }
 
     suspend fun addToFavorites(stock: Stock) {
         stock.isFavorite = true
-        stockDao.updateItem(stock)
+        stockDao.updateStock(stock)
     }
 
-    suspend fun updateItem(stock: Stock) {
-        stockDao.updateItem(stock)
-    }
+    suspend fun updateItem(stock: Stock) = stockDao.updateStock(stock)
 
-    suspend fun
-
+    suspend fun getRemoteStockData(stockTicker: String): Resource<StockRemoteModel> =
+        remoteDataSource.getStockData(stockTicker)
 }
