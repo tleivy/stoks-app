36f89f206797b11befd3eb7c995d12391f9bbffc
tleivy <tomerleivy1@gmail.com>
2023-12-01 17:01:37 +0200

    fetch API token

--- app/src/main/java/com/example/stoks/data/remote_db/StockRemoteDataSource.kt
+++ app/src/main/java/com/example/stoks/data/remote_db/StockRemoteDataSource.kt
@@ -2,6 +2,7 @@ package com.example.stoks.data.remote_db
 
 import com.example.stoks.data.model.StockRemoteModel
 import com.example.stoks.data.utils.Resource
+import com.example.stoks.data.utils.Constants
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,7 +11,7 @@ class StockRemoteDataSource @Inject constructor(
     private val stockService: StockService
 ) : BaseDataSource() {
 
-    suspend fun getStockData(stockTicker: String, apiToken: String): Resource<StockRemoteModel> {
-        return getResult { stockService.getQuote(stockTicker, apiToken) }
+    suspend fun getStockData(stockTicker: String): Resource<StockRemoteModel> {
+        return getResult { stockService.getQuote(stockTicker, Constants.API_KEY) }
     }
 }
