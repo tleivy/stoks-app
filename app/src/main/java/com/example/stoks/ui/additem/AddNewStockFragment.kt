7cadde2b1050a623a936deaf26d6896401621814
tleivy <tomerleivy1@gmail.com>
2023-12-02 22:34:36 +0200

    architecture re-haul

--- app/src/main/java/com/example/stoks/ui/additem/AddNewStockFragment.kt
+++ app/src/main/java/com/example/stoks/ui/additem/AddNewStockFragment.kt
@@ -4,57 +4,46 @@ import android.content.ContentResolver
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.stoks.R
-import com.example.stoks.databinding.AddItemFragmentBinding
-import com.example.stoks.ui.ItemViewModel
+import com.example.stoks.databinding.AddNewStockFragmentBinding
 import com.example.stoks.data.local.StocksDataMaps
-import com.example.stoks.data.model.StockLocalModel
 import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AddNewStockFragment : Fragment() {
-    private var _binding: AddItemFragmentBinding? = null
+    private var _binding: AddNewStockFragmentBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel: ItemViewModel by activityViewModels()
+    private val viewModel: AddNewStockViewModel by activityViewModels()
 
-    private var imageUri: Uri? = null
+    private var lastUiState: AddNewStockViewModel.AddNewStockUiState? = null
 
-
-    val pickItemLauncher: ActivityResultLauncher<Array<String>> =
-        registerForActivityResult(ActivityResultContracts.OpenDocument()) {
-            if (it != null) {
-                binding.previewImage.setImageURI(it)
-                requireActivity().contentResolver.takePersistableUriPermission(
-                    it!!,
-                    Intent.FLAG_GRANT_READ_URI_PERMISSION
-                )
-                imageUri = it
-            } else {
-                Toast.makeText(requireContext(), R.string.selectImage, Toast.LENGTH_SHORT).show()
-            }
+    private val pickItemLauncher =
+        registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+            viewModel.onImageSelected(uri)
         }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = AddItemFragmentBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = AddNewStockFragmentBinding.inflate(inflater, container, false)
 
-        val stockSymbols = StocksDataMaps.stockSymbols
+        val stockTickers = StocksDataMaps.stockTickers
         val stockImages = StocksDataMaps.stockImages
         var companyName = ""
 
@@ -63,84 +52,20 @@ class AddNewStockFragment : Fragment() {
             val selectedChip = group.findViewById<Chip>(checkedId)
             val selectedChipText = selectedChip?.text.toString()
             companyName = selectedChipText
-            if (stockSymbols.containsKey(companyName)) {
-                binding.stockSymbol.setText(stockSymbols[companyName])
+            if (stockTickers.containsKey(companyName)) {
+                binding.stockTicker.setText(stockTickers[companyName])
                 binding.stockName.setText(companyName)
                 Glide.with(this).load(stockImages[companyName]!!).into(binding.previewImage)
-                imageUri = null
+                viewModel.nullifyImageUri()
             }
         }
-
-        binding.addBtn.setOnClickListener {
-            //check fields
-            var allFilled = true
-            if (TextUtils.isEmpty(binding.stockAmount.text?.toString())) {
-                allFilled = false
-                binding.stockAmount.error = getString(R.string.fillAmount)
-                return@setOnClickListener
-            }
-            if (TextUtils.isEmpty(binding.stockPrice.text?.toString())) {
-                allFilled = false
-                binding.stockPrice.error = getString(R.string.fillPrice)
-                return@setOnClickListener
-
-            }
-            val selectedChipText =
-                binding.chipGroup.findViewById<Chip>(binding.chipGroup.checkedChipId)?.text?.toString()
-            if (selectedChipText.isNullOrEmpty()) {
-                allFilled = false
-                Toast.makeText(requireContext(), R.string.fillStock, Toast.LENGTH_SHORT)
-                    .show()
-                return@setOnClickListener
-            }
-
-            var tempstring: Uri?
-            if (imageUri != null) {
-                tempstring = imageUri
-            } else {
-                val resourceId: Int? = stockImages[companyName]
-                tempstring = Uri.parse(
-                    ContentResolver.SCHEME_ANDROID_RESOURCE +
-                            "://" + resourceId?.let { it1 -> resources.getResourcePackageName(it1) } +
-                            "/" + resourceId?.let { it1 -> resources.getResourceTypeName(it1) } +
-                            "/" + resourceId?.let { it1 -> resources.getResourceEntryName(it1) }
-                )
-            }
-
-            if (allFilled) {
-                val item = StockLocalModel(
-                    binding.stockName.text.toString(),
-                    binding.stockSymbol.text.toString(),
-                    binding.stockPrice.text.toString().toDouble(),
-                    binding.stockAmount.text.toString().toLong(),
-                    tempstring,
-                    0.0,
-                    false
-                )
-                viewModel.addItem(item)
-                findNavController().navigate(R.id.action_addItemFragment_to_allItemsFragment)
-            }
-        }
-
-        binding.resetBtn.setOnClickListener {
-            imageUri = null
-            binding.stockName.setText("")
-            binding.chipGroup.clearCheck()
-            binding.stockSymbol.setText("")
-            binding.stockPrice.setText("")
-            binding.stockAmount.setText("")
-            binding.previewImage.setImageResource(R.drawable.app_logo)
-        }
-
-        binding.imageBtn.setOnClickListener {
-            pickItemLauncher.launch(arrayOf("image/*"))
-        }
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        registerObservers()
+        initListeners()
     }
 
     override fun onDestroyView() {
@@ -148,5 +73,142 @@ class AddNewStockFragment : Fragment() {
         _binding = null
     }
 
+    private fun registerObservers() = with(viewModel) {
+        uiState.observe(viewLifecycleOwner) { newState ->
+            val oldState = lastUiState
+            if (oldState != null) {
+                if (newState.imageUri != oldState.imageUri) {
+                    if (newState.imageUri != null) {
+                        binding.previewImage.setImageURI(newState.imageUri)
+                        requireActivity().contentResolver.takePersistableUriPermission(
+                            newState.imageUri!!,
+                            Intent.FLAG_GRANT_READ_URI_PERMISSION
+                        )
+                    }
+                }
+                if (newState.errorMessage != oldState.errorMessage) {
+                    if (newState.errorMessage != null) {
+                        Toast.makeText(requireContext(), newState.errorMessage, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+                if (newState.appAddedSuccessfully != oldState.appAddedSuccessfully) {
+                    if (newState.appAddedSuccessfully == true) {
+                        findNavController().navigate(R.id.action_addItemFragment_to_allItemsFragment)
+                    }
+                }
+            }
+            lastUiState = newState
+        }
+    }
 
+    private fun initListeners() {
+
+        binding.stockTicker.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(inputTicker: Editable?) {
+                viewModel.updateStockTicker(inputTicker.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not needed
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not needed
+            }
+        })
+
+        binding.boughtAmount.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(inputAmount: Editable?) {
+                viewModel.updateBoughtAmount(inputAmount.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not needed
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not needed
+            }
+        })
+
+        binding.boughtPrice.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(inputPrice: Editable?) {
+                viewModel.updateBoughtPrice(inputPrice.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not needed
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Not needed
+            }
+        })
+
+        binding.addStockButton.setOnClickListener {
+            if (checkIfAllFieldsAreFilled()) {
+                val uriString: Uri? = createImageUriString()
+                viewModel.onAddStockButtonClicked(
+                    binding.stockName.toString(),
+                    binding.stockTicker.toString(),
+                    binding.boughtAmount.text.toString().toLong(),
+                    binding.boughtPrice.text.toString().toDouble(),
+                    uriString
+                )
+            }
+        }
+
+        binding.resetButton.setOnClickListener {
+            viewModel.nullifyImageUri()
+            binding.stockName.setText("")
+            binding.chipGroup.clearCheck()
+            binding.stockTicker.setText("")
+            binding.boughtPrice.setText("")
+            binding.boughtAmount.setText("")
+            binding.previewImage.setImageResource(R.drawable.app_logo)
+        }
+
+        binding.addImageButton.setOnClickListener {
+            pickItemLauncher.launch(arrayOf("image/*"))
+        }
+    }
+
+    private fun checkIfAllFieldsAreFilled(): Boolean {
+        if (TextUtils.isEmpty(binding.boughtAmount.text?.toString())) {
+            binding.boughtAmount.error = getString(R.string.fillAmount)
+            return false
+        }
+        if (TextUtils.isEmpty(binding.boughtPrice.text?.toString())) {
+            binding.boughtPrice.error = getString(R.string.fillPrice)
+            return false
+        }
+        val selectedChipText =
+            binding.chipGroup.findViewById<Chip>(binding.chipGroup.checkedChipId)?.text?.toString()
+        if (selectedChipText.isNullOrEmpty()) {
+            Toast.makeText(requireContext(), R.string.fillStock, Toast.LENGTH_SHORT)
+                .show()
+            return false
+        }
+        return true
+    }
+
+    private fun createImageUriString(): Uri? {
+        val tempString: Uri? = if (viewModel.uiState.value?.imageUri != null) {
+            viewModel.uiState.value?.imageUri
+        } else {
+            val resourceId: Int? = StocksDataMaps.stockImages[binding.stockName.text.toString()]
+            Uri.parse(
+                ContentResolver.SCHEME_ANDROID_RESOURCE +
+                        "://" + resourceId?.let { it1 -> resources.getResourcePackageName(it1) } +
+                        "/" + resourceId?.let { it1 -> resources.getResourceTypeName(it1) } +
+                        "/" + resourceId?.let { it1 -> resources.getResourceEntryName(it1) }
+            )
+        }
+        return tempString
+    }
+
+    private fun nullifyImageUri() {
+        viewModel.nullifyImageUri()
+    }
 }
--- app/src/main/java/com/example/stoks/ui/additem/AddNewStockViewModel.kt
+++ app/src/main/java/com/example/stoks/ui/additem/AddNewStockViewModel.kt
@@ -1,10 +1,19 @@
 package com.example.stoks.ui.additem
 
+import android.net.Uri
+import androidx.annotation.StringRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.stoks.R
+import com.example.stoks.data.model.StockLocalModel
+import com.example.stoks.data.model.StockRemoteModel
 import com.example.stoks.data.repository.StockRepository
+import com.example.stoks.data.utils.Resource
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,11 +24,121 @@ class AddNewStockViewModel @Inject constructor(
     private val _uiState: MutableLiveData<AddNewStockUiState> = MutableLiveData()
     val uiState: LiveData<AddNewStockUiState> = _uiState
 
+    fun updateStockTicker(newTicker: String) {
+        val updatedState = _uiState.value?.copy(stockTicker = newTicker) ?: AddNewStockUiState(
+            selectedChip = null,
+            stockTicker = newTicker,
+            boughtAmount = "",
+            boughtPrice = "",
+            imageUri = null,
+            errorMessage = null
+        )
+        _uiState.value = updatedState
+    }
 
+    fun updateBoughtAmount(newAmount: String) {
+        val updatedState = _uiState.value?.copy(boughtAmount = newAmount) ?: AddNewStockUiState(
+            selectedChip = null,
+            stockTicker = "",
+            boughtAmount = newAmount,
+            boughtPrice = "",
+            imageUri = null,
+            errorMessage = null
+        )
+        _uiState.value = updatedState
+    }
 
+    fun updateBoughtPrice(newPrice: String) {
+        val updatedState = _uiState.value?.copy(boughtPrice = newPrice) ?: AddNewStockUiState(
+            selectedChip = null,
+            stockTicker = "",
+            boughtAmount = "",
+            boughtPrice = newPrice,
+            imageUri = null,
+            errorMessage = null
+        )
+        _uiState.value = updatedState
+    }
+
+    fun onImageSelected(uri: Uri?) {
+        val updatedState: AddNewStockUiState
+        if (uri != null) {
+            updatedState = _uiState.value?.copy(imageUri = uri) ?: AddNewStockUiState(
+                selectedChip = null,
+                stockTicker = "",
+                boughtAmount = "",
+                boughtPrice = "",
+                imageUri = uri,
+                errorMessage = null
+            )
+        } else {
+            updatedState =
+                _uiState.value?.copy(errorMessage = R.string.selectImage) ?: AddNewStockUiState(
+                    selectedChip = null,
+                    stockTicker = "",
+                    boughtAmount = "",
+                    boughtPrice = "",
+                    imageUri = null,
+                    errorMessage = R.string.selectImage
+                )
+        }
+        _uiState.value = updatedState
+    }
+
+    fun nullifyImageUri() {
+        val updatedState = _uiState.value?.copy(imageUri = null) ?: AddNewStockUiState(
+            selectedChip = null,
+            stockTicker = "",
+            boughtAmount = "",
+            boughtPrice = "",
+            imageUri = null,
+            errorMessage = null
+        )
+        _uiState.value = updatedState
+    }
+
+    fun onAddStockButtonClicked(
+        companyName: String,
+        stockTicker: String,
+        boughtAmount: Long,
+        boughtPrice: Double,
+        companyImage: Uri?
+    ) = viewModelScope.launch {
+
+        val updatedExistingStock =
+            repository.addToOwnedShares(companyName, boughtAmount, boughtPrice)
+
+        val remoteServiceResource: Resource<StockRemoteModel> =
+            repository.fetchRemoteStockData(stockTicker)
+
+        if (!updatedExistingStock) {
+            val currentPrice = remoteServiceResource.status.data?.currentPrice ?: 0.0
+            val newStock = StockLocalModel(
+                companyName, stockTicker, boughtPrice,
+                currentPrice, boughtAmount, companyImage, false
+            )
+            repository.addNewStock(newStock)
+        }
+        _uiState.value = AddNewStockUiState(
+            selectedChip = null,
+            stockTicker = "",
+            boughtAmount = "",
+            boughtPrice = "",
+            imageUri = null,
+            errorMessage = null,
+            appAddedSuccessfully = true
+        )
+    }
 
     data class AddNewStockUiState(
-        val error: String? = null
+        val selectedChip: Chip? = null,
+        val stockTicker: String? = null,
+        val boughtAmount: String? = null,
+        val boughtPrice: String? = null,
+        var imageUri: Uri? = null,
+        @StringRes val errorMessage: Int? = null,
+        val appAddedSuccessfully: Boolean? = null
     )
+
 
 }
