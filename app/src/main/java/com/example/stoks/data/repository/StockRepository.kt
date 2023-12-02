c3a6c51cb8091ee24d2c56f73ab19f8f5a2fe98e
tleivy <tomerleivy1@gmail.com>
2023-12-02 14:50:13 +0200

    function updates

--- app/src/main/java/com/example/stoks/data/repository/StockRepository.kt
+++ app/src/main/java/com/example/stoks/data/repository/StockRepository.kt
@@ -1,11 +1,15 @@
 package com.example.stoks.data.repository
 
+import android.util.Log
 import com.example.stoks.data.local.StockLocalDataSource
 import com.example.stoks.data.model.StockLocalModel
 import com.example.stoks.data.model.StockRemoteModel
 import com.example.stoks.data.remote_db.StockRemoteDataSource
+import com.example.stoks.data.utils.Loading
 import com.example.stoks.data.utils.Resource
 import com.example.stoks.data.utils.Success
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 
@@ -17,38 +21,75 @@ class StockRepository @Inject constructor(
 
     fun getFavoriteStocks() = localDataSource.getFavoriteStocks()
 
-    suspend fun getStockByName(name: String) = localDataSource.getStockByName(name)
+    suspend fun getStockByName(name: String) {
+        withContext(Dispatchers.IO) {
+            localDataSource.getStockByName(name)
+        }
+    }
 
-    suspend fun getStockByTicker(ticker: String) = localDataSource.getStockByName(ticker)
+    suspend fun getStockByTicker(ticker: String): StockLocalModel {
+        withContext(Dispatchers.IO) {
+            localDataSource.getStockByTicker(ticker)
+        }
+    }
 
-    suspend fun addNewStock(stock: StockLocalModel) = localDataSource.addNewStock(stock)
+    suspend fun addNewStock(stock: StockLocalModel) {
+        withContext(Dispatchers.IO) {
+            localDataSource.addNewStock(stock)
+        }
+    }
 
-    suspend fun deleteStock(stock: StockLocalModel) = localDataSource.deleteStock(stock)
+    suspend fun deleteStock(stock: StockLocalModel) {
+        withContext(Dispatchers.IO) {
+            localDataSource.deleteStock(stock)
+        }
+    }
 
-    suspend fun deleteAllStocks() = localDataSource.deleteAllStocks()
+    suspend fun deleteAllStocks() {
+        withContext(Dispatchers.IO) {
+            localDataSource.deleteAllStocks()
+        }
+    }
 
-    suspend fun updateStockData(stock: StockLocalModel) = localDataSource.updateStockData(stock)
+    suspend fun updateStockData(stock: StockLocalModel) {
+        withContext(Dispatchers.IO) {
+            localDataSource.updateStockData(stock)
+        }
+    }
 
     suspend fun addToFavorites(stock: StockLocalModel) {
         stock.isFavorite = true
-        localDataSource.updateStockData(stock)
+        updateStockData(stock)
     }
 
     suspend fun removeFromFavorites(stock: StockLocalModel) {
         stock.isFavorite = false
-        localDataSource.updateStockData(stock)
+        updateStockData(stock)
     }
 
-    suspend fun getRemoteStockData(stockTicker: String): Resource<StockRemoteModel> {
-        val remoteStockDataResource: Resource<StockRemoteModel> =
-            remoteDataSource.getStockData(stockTicker)
-        if (remoteStockDataResource.status is Success) {
-            val localStockData = getStockByTicker(stockTicker)
-            val updatedStockData = localStockData.copy(
-                currentPrice = remoteStockDataResource.status.data?.currentPrice
+    suspend fun updateStockOwnedAmount(stockName: String, newAmount: Long) {
+        withContext(Dispatchers.IO) {
+            val localStockData = localDataSource.getStockByName(stockName)
+            localStockData.ownedAmount = newAmount
+            localDataSource.updateStockData(localStockData)
+        }
+    }
+
+    /**
+     * This function fetches the up-to-date stock data from a remote API.
+     * It then updates the current price of the requested stock and
+     * returns the fetched data.
+     * **/
+    suspend fun fetchRemoteStockData(stockTicker: String): Resource<StockRemoteModel> {
+        val remoteStockDataResource: Resource<StockRemoteModel>
+        withContext(Dispatchers.IO) {
+            remoteStockDataResource = remoteDataSource.getStockData(stockTicker)
+            if (remoteStockDataResource.status is Success) {
+                val localStockData = getStockByTicker(stockTicker)
+                localStockData.currentPrice = remoteStockDataResource.status.data?.currentPrice
                     ?: localStockData.currentPrice
-            )
-            updateStockData(updatedStockData)
+                updateStockData(localStockData)
+            }
         }
         return remoteStockDataResource
     }
